056c39d3b6a93d3fc25b6872463c4ddcd4537f5a
Thalles Daniel <106764514+ThallesDaniel@users.noreply.github.com>
2024-11-30 21:58:26 -0300

    implementação do metodo pix

---
+++ src/main/java/com/thallesdaniel/paymentsystem/client/PixClient.java
@@ -0,0 +1,19 @@
+package com.thallesdaniel.paymentsystem.client;
+
+import org.springframework.stereotype.Component;
+
+import com.thallesdaniel.paymentsystem.dto.PixPaymentRequest;
+
+@Component
+public class PixClient {
+
+    public boolean enviarPix(PixPaymentRequest request) {
+        // Simulação de envio para o sistema PIX (para repositorio de demonstração, interessado integrar com api do banco de interesse)
+        System.out.println("Enviando PIX para chave: " + request.getChavePixDestinatario());
+        System.out.println("Valor: R$ " + request.getValor());
+        System.out.println("Descrição: " + request.getDescricao());
+
+        // Simula sucesso ou falha
+        return true; // ou false em caso de erro
+    }
+}
